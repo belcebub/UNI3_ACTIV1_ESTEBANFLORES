9273ce9735771aa3df169a3139e2d9484d2a06c1
Gerardo Flores <gera.flores98@hotmail.com>
2023-03-27 23:56:12 -0600

    Primera clase llamada electrodomestico casi terminada

--- src/src/Electrodomestico.java
+++ src/src/Electrodomestico.java
@@ -15,10 +15,24 @@ public class Electrodomestico {
     public final static char CONSUMO_DEF = 'F';
     public final static int PESO_DEF = 5;
     
-    float precioBase;
-    String color;
-    char consEnergetico;
-    int peso;
+    private float precioBase;
+    private String color;
+    private char consEnergetico;
+    private int peso;
+
+    public Electrodomestico() {
+        this.precioBase = PRECIO_DEF;
+        this.color = COLOR_DEF;
+        this.consEnergetico = CONSUMO_DEF;
+        this.peso = PESO_DEF;
+    }
+
+    public Electrodomestico(float precioBase, int peso) {
+        this.color = COLOR_DEF;
+        this.consEnergetico = CONSUMO_DEF;
+        this.precioBase = precioBase;
+        this.peso = peso;
+    }
 
     public Electrodomestico(float precioBase, String color, char consEnergetico, int peso) {
         this.precioBase = precioBase;
@@ -26,6 +40,46 @@ public class Electrodomestico {
         this.consEnergetico = consEnergetico;
         this.peso = peso;
     }
+
+    public float getPrecioBase() {
+        return precioBase;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public char getConsEnergetico() {
+        return consEnergetico;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
     
+    public void comprobarConsumoEnergetico(char letra){
+        if(letra > 'A' && letra < 'F'){
+            this.consEnergetico = letra;
+        }
+        this.consEnergetico = CONSUMO_DEF;
+    }
     
+    public void comprobarColor(String color){
+        if (color.equals("negro") || color.equals("NEGRO")){
+            this.color = color;
+        } else {
+            if(color.equals("ROJO") || color.equals("rojo")){
+                this.color = color;
+            } else {
+                if(color.equals("azul") || color.equals("AZUL")){
+                    this.color = color;
+                } else {
+                    if(color.equals("gris") || color.equals("GRIS")){
+                       this.color = color; 
+                    }
+                }
+            } 
+        }
+        this.color = COLOR_DEF;
+    } 
 }
